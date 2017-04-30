89ff2785356483a0cfd5cd75a0ece35b033e35bd
Jiun Wang <jiuntwang@gmail.com>
2017-04-30 00:08:47 -0700

    Initial buildcontrol setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theSunnySideWeatherAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the theSunnySideWeatherAppApp
+ */
+angular.module('theSunnySideWeatherAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
