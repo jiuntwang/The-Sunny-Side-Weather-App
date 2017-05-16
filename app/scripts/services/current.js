cb5ff2a879243ddb806f555cf7faf8bb5a652c78
Jiun Wang <jiuntwang@gmail.com>
2017-05-16 11:12:24 -0700

    Added API key

--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -12,10 +12,9 @@ angular.module('theSunnySideWeatherAppApp')
     // Service logic
     // ...
 
-    var meaningOfLife = 42;
 
     // Public API here
-    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=', {}, {
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=260156459874096ef4f2b7fcb520a496', {}, {
       query: {
         method:'GET',
         params:{
